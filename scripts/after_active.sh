dc49867cd3fb37decd9e6ef910c181c9af89636c
Hugo Carrizo Manzoni <41146153+StrikerArg@users.noreply.github.com>
2024-08-27 10:01:12 +0000

    Update after_active.sh

--- scripts/after_active.sh
+++ scripts/after_active.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo systemctl restart apache2
-sudo rm -rf /tmp/config /tmp/scripts /tmp/templates /tmp/appspec.yml
+#sudo rm -rf /tmp/config /tmp/scripts /tmp/templates /tmp/appspec.yml
